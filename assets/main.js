7f554345c338aa145d888a94ed71409859a5941a
demisfwebs <demisfermincontact@gmail.com>
2024-02-29 00:42:53 -0400

    first commit

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    var ctn = document.getElementById('carga-ctn')
+    ctn.style.transform = "translateY(-100vh)"}
+
+// document.querySelector(".fa-chevron-down").addEventListener("click", () => {
+//  window.scrollTo(0, 680);
+// })
